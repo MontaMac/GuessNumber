3e473002d96dc977f3799549f4852bdf4ee088aa
MontaMac <monta.pole@inbox.lv>
2018-09-04 19:38:38 +0300

    commit 4sep2018

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,6 +10,8 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
+        String answer;
+        do{
         int myNum = rand.nextInt(100) + 1;
         System.out.println(myNum);
 
@@ -28,7 +30,25 @@ public class Main {
             }
         }
         if (userLost == true) {
-            System.out.println ("you are lost");
+            System.out.println("you are lost");
         }
+        System.out.println("Do you want to play again (y/n)?");
+        answer = askYN();
+    }while (answer.equals ("y"));
+        System.out.println("good bye");
+
+    }
+    static String askYN(){
+        String answer;
+        do {
+            answer = scan.next();
+            if (!answer.equals ("y") && !answer.equals ("n")){
+                System.out.println("You can enter only y or n");
+                continue;
+            } else{
+                break;
+            }
+        } while (true);
+        return answer;
     }
 }
